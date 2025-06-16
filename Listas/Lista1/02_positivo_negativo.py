47d2dad205d0a2fbd6d03c061780efc234e31f4f
Joao Torre <joaogabrieltorre@gmail.com>
2025-06-16 19:45:02 -0300

    listas de exercicios

--- Listas/Lista1/02_positivo_negativo.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Informe um numero: '))
-
-if (num > 0):
-    print(num, 'eh positivo')
-elif (num < 0):
-    print(num, 'eh negativo')
-else:
-    print('O numero eh igual a 0')
--- Listas/Lista1/03_vogais_consoantes.py
+++
@@ -1,6 +0,0 @@
-letra = input('Informe uma letra: ')
-
-if ('AEIOU'.find(letra.upper()) >= 0):
-    print('VOGAL')
-else:
-    print('CONSOANTE')
